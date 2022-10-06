0f3a9a1a8248b70a9f6389abf6882b003f70211f
RiHoude <112140293+RiHoude@users.noreply.github.com>
2022-10-05 23:50:24 -0700

    1st

---
+++ A63.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int getSum(int N);
+
+int main () {
+  int n;
+  cout << "Enter the total amount of numbers\n";
+  cin >> n;
+  
+  cout << getSum(n) << endl;
+  return 0;
+}
+
+int getSum(int N) {
+  int i, num, sum; 
+  int min, max = 0;
+  for (i = 0; i < N; i++) {
+      cout << "Enter a number\n";
+      cin >> num;
+      sum += num;
+      if (num > max) {
+          max = num;
+      }
+      if (num < min) {
+          min = num;
+      }
+  }
+  sum = sum - max - min;
+  return sum;
+}
